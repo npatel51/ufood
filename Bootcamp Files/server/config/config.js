62ac4aee3cc715e9159ca2f95238309d32dfbe96
jzhuang01 <jzhuang01@ufl.edu>
2018-10-16 09:48:49 -0400

    edited correct config file and added Procfile

--- Bootcamp Files/server/config/config.js
+++ Bootcamp Files/server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     db: {
       uri: 'mongodb://CEN3031:CEN3031TA@ds155252.mlab.com:55252/softdev-ass3', //place the URI of your mongo database here.
     }, 
-    port: 8080
+    port: process.env.PORT || 8080
   };
--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
     db: {
       uri: '/*TODO: mongo URI here*/', 
     }, 
-    port: process.env.PORT || 8080,
+    port: 8080,
     token: 'pk.eyJ1IjoiZm9vZGJhYnkxIiwiYSI6ImNqbjRuOXluYTByN3Uza3Fvc2xuOTAzaXMifQ.E2kQtGJ19Y6ofltNnZaa3w'
   };
