af20e31dd208f55522a8a22a07359c2de8a6f155
Niraj <npatel3@ufl.edu>
2018-10-21 12:56:55 -0400

    crud functions for events

---
+++ client/js/controllers/eventsController.js
@@ -0,0 +1,55 @@
+angular.module('events').controller('EventsController', ['$scope', 'Events', 
+  function($scope, Events) {
+    /* Get all the events, then bind it to the scope */
+    Events.getAll().then(function(response) {
+      $scope.events = response.data;
+    }, function(error) {
+      console.log('Unable to retrieve events:', error);
+    });
+
+    $scope.detailedInfo = undefined;
+    $scope.showDetail = false;
+
+    $scope.addEvent = function() {
+      //create a new event object
+      let newEvent = {
+        title:$scope.title,
+        date:$scope.date,        
+        time:$scope.time,         
+        description:$scope.description,  
+        address:$scope.address,      
+        typeOfFood:$scope.typeOfFood 
+       };  
+
+    Events.create(newEvent).then(function(response){
+        Events.getAll().then(function(response) {
+          $scope.events = response.data;
+        }, function(error) {
+          console.log('Unable to retrieve events:', error);
+        });
+      },function(error){
+        console.log('Unable to add new event:', error);
+      });
+      
+    };
+
+    $scope.deleteEvent = function($event,index) {
+      $event.stopPropagation();
+      Events.delete($scope.events[index]).then(function(response){
+         //if delete was succesfull, then redirect to listing.list which retrieves all the events
+         Events.getAll().then(function(response) {
+          $scope.events = response.data;
+        }, function(error) {
+          console.log('Unable to retrieve events:', error);
+        });
+      },function(error){
+         console.log('Unable to remove listing:',error);
+      });
+    
+    };
+
+    $scope.showDetails = function(index) {
+        $scope.detailedInfo = $scope.events[index];
+    };
+  }
+]);
